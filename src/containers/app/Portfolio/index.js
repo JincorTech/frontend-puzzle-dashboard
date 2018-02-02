7cb76857156596435f9ed86ee2b3c8675683e65c
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-02 16:26:28 +0500

    Closes #2 Add table

--- src/containers/app/Portfolio/index.js
+++ src/containers/app/Portfolio/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
 
-export default () => (<div>portfolio</div>);
+import PortfolioTable from '../../../components/app/PortfolioTable';
+
+import s from './styles.css';
+
+const Portfolio = (props) => {
+  console.log(props);
+
+  return (
+    <div className={s.layout}>
+      <div className={s.row}>
+        <div className={s.summary}></div>
+        <div className={s.chart}></div>
+      </div>
+
+      <div className={s.table}>
+        <PortfolioTable/>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
